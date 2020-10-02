02c7ca7344b26c0fc337344d164e9a8d7fafe7f2
Sam Patton <sam_patton@hotmail.com>
2020-10-02 10:03:08 -0700

    Setting up new pc

---
+++ src/Blink1/Blink1/Debug/VM_DBG/VM_mem_check_sam.cpp.d
@@ -0,0 +1,2 @@
+C:\Users\sam_p\AppData\Local\Temp\VMBuilds\Blink1\uno\Debug\VM_DBG\VM_mem_check_sam.cpp.o: \
+ c:\program\ files\ (x86)\microsoft\ visual\ studio\2019\community\common7\ide\extensions\taznonos.td4\Micro\ Platforms\default\debuggers\VM_DBG\VM_mem_check_sam.cpp
